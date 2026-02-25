566113ed1e15ddabb234cd05613a8afe088423e3
novahbyshahzaib <studentxeniya@gmail.com>
2026-02-25 14:01:16 +0530

    Create Color.kt

---
+++ app/src/main/java/com/snoahtune/app/ui/theme/Color.kt
@@ -0,0 +1,13 @@
+package com.snoahtune.app.ui.theme
+
+import androidx.compose.ui.graphics.Color
+
+val Background    = Color(0xFFF5F0E8)
+val ElectricYellow = Color(0xFFFFE500)
+val HotPink       = Color(0xFFFF2D78)
+val ElectricBlue  = Color(0xFF0038FF)
+val LimeGreen     = Color(0xFF00FF85)
+val BorderBlack   = Color(0xFF000000)
+val TextPrimary   = Color(0xFF000000)
+val TextSecondary = Color(0xFF3A3A3A)
+val SurfaceWhite  = Color(0xFFFFFFFF)
