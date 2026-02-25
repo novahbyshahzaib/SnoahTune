eb0ca2490058c34e64376e020fdbe9729f512b47
novahbyshahzaib <studentxeniya@gmail.com>
2026-02-25 13:55:35 +0530

    Create MusicService.kt

---
+++ app/src/main/java/com/snoahtune/app/service/MusicService.kt
@@ -0,0 +1,41 @@
+package com.snoahtune.app.service
+
+import android.content.Intent
+import androidx.annotation.OptIn
+import androidx.media3.common.util.UnstableApi
+import androidx.media3.exoplayer.ExoPlayer
+import androidx.media3.session.MediaSession
+import androidx.media3.session.MediaSessionService
+import dagger.hilt.android.AndroidEntryPoint
+
+@AndroidEntryPoint
+class MusicService : MediaSessionService() {
+
+    private var mediaSession: MediaSession? = null
+
+    @OptIn(UnstableApi::class)
+    override fun onCreate() {
+        super.onCreate()
+        val player = ExoPlayer.Builder(this).build()
+        mediaSession = MediaSession.Builder(this, player)
+            .setId("SnoahTuneSession")
+            .build()
+    }
+
+    override fun onGetSession(controllerInfo: MediaSession.ControllerInfo): MediaSession? =
+        mediaSession
+
+    override fun onTaskRemoved(rootIntent: Intent?) {
+        val player = mediaSession?.player
+        if (player == null || !player.playWhenReady || player.mediaItemCount == 0) stopSelf()
+    }
+
+    override fun onDestroy() {
+        mediaSession?.run {
+            player.release()
+            release()
+            mediaSession = null
+        }
+        super.onDestroy()
+    }
+}
